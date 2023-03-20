695cfded3f5c1b965b91358044e2bdaab476ee2c
Tarun <tggupta1@gmail.com>
2023-03-20 20:18:47 +0530

    Comment 1

--- cypress/e2e/DemoBlaze.cy.js
+++ cypress/e2e/DemoBlaze.cy.js
@@ -2,6 +2,8 @@ describe("Demo Blaze Automation", () => {
   let userdata
   let mysession
 
+  //Comment 1
+
   before(function () {
     cy.fixture('demoBlaze_NewMessage.json').then(function (data) {
       userdata = data;
